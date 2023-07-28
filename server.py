995ea0fc9cb09690015836047c7da2565959f374
mohan <mohanthosare@gmail.com>
2023-07-28 14:47:13 +0530

    jenkins pipeline

---
+++ server.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/",methods=["GET"])
+def root():
+     return "<h1>Welcome to python application</h1>"
+
+app.run(host="0.0.0.0", port=4000, debug=True)
